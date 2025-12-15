4696eccd24237a863de5a461dd67bcd0352547f8
lizhenyu <2945659544@qq.com>
2025-12-15 10:38:48 +0800

    first commit

---
+++ OperaMaster/View/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using HandyControl.Tools;
+
+namespace OperaMaster
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : HandyControl.Controls.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ConfigHelper.Instance.SetWindowDefaultStyle();
+        }
+    }
+}
