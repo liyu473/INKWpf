1238b4cb6645caab7cbeda7955e6eddef53b70ec
lizhenyu <2945659544@qq.com>
2025-12-16 17:07:10 +0800

    补充

--- OperaMaster/View/SettingsView.xaml.cs
+++ OperaMaster/View/SettingsView.xaml.cs
@@ -1,15 +1,15 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using OperaMaster.ViewModel;
 
 namespace OperaMaster.View;
 
 public partial class SettingsView : UserControl
 {
-    private readonly SettingsViewModel _vm;
     public SettingsView(SettingsViewModel vm)
     {
         InitializeComponent();
         DataContext = vm;
-        _vm = vm;
     }
 }
